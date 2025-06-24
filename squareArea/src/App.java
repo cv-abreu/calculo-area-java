d0b0f462be68dbf2a8d3f35530699cfbaed58085
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2025-06-24 14:47:08 -0300

    Update App.java

--- squareArea/src/App.java
+++ squareArea/src/App.java
@@ -12,43 +12,40 @@ public class App {
         System.out.println("> 1. Quadrado \n 2. Retângulo \n 3. Triângulo \n 4. Círculo  ");
         int poligono = scanner.nextInt();
 
-        if (poligono == 1) {
+        switch(poligono){
+            case 1:
             System.out.println("> Insira o lado do quadrado: ");
             var side = scanner.nextFloat();
             var areaSquare = side*side;
             System.out.printf("> A área do quadrado é: %.2f metros quadrados", areaSquare);
+            break;
 
-        } else if (poligono == 2) {
-
+            case 2:
             System.out.println("> Insira o comprimento do retângulo: ");
             var comp = scanner.nextFloat();
             System.out.println("> Insira a altura do retângulo:");
-            var altura = scanner.nextFloat();
-            var areaRetangle = comp * altura;
-
+            var largura = scanner.nextFloat();
+            var areaRetangle = comp * largura;
             System.out.printf("> A área do retângulo é: %.2f metros quadrados ", areaRetangle);
+            break;
 
-
-        } else if  (poligono ==  3){
-
+            case 3:
             System.out.println("> Insira a altura do triângulo:");
             var altura = scanner.nextFloat();
             System.out.println("> Insira a base do triângulo:");
             var base = scanner.nextFloat();
             var areaTriangle = (base * altura) /2;
-
             System.out.printf("> A área do triângulo é: %.2f metros quadrados" , areaTriangle);
+            break;
 
-        
-        } else if (poligono ==4) {
-
+            case 4:
             System.out.println("> Insira raio do Círculo:");
             var raio = scanner.nextFloat();
             var areaCircle = 3.14 * (raio * raio);
-            
             System.out.printf("> A área do círculo é: %.2f metros quadrados", areaCircle);
+            break;
 
-        } else {
+            default:
             System.out.println("> Erro; reinicializar");
         }
         
